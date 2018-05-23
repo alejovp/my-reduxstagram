53a75270d2990d1924fa0122880add57703985c2
Alejandro Vazquez <alejandro.vazquez@ext.wanup.com>
2018-05-23 14:43:09 +0200

    React router created

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
-class Main extends Component {
-
-    render() {
-        return (
-            <div>
-                <h1>
-                    <Link to="/">My Reduxstagram</Link>
-                </h1>
-            </div>
-        );
-    }
-}
+const Main = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>
+          <Link to="/">My Reduxstagram</Link>
+        </h1>
+        {React.cloneElement(this.props.children, this.props)}
+      </div>
+    );
+  }
+});
 
 export default Main;
