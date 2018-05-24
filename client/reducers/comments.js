1d8bda97907e6bc0a76499fa4f4fbfb434e60c32
Alejandro Vazquez <alejandro.vazquez@ext.wanup.com>
2018-05-24 15:22:16 +0200

    Comments reducer composition to handle single comment actions

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,37 @@
+function postComments(state = [], action) {
+    switch (action.type) {
+        case 'ADD_COMMENT':
+            // return new state with the new comment
+            return [
+                // the existing comments
+                ...state,
+                // plus the new one
+                { user: action.author, text: action.comment }
+            ];
+        case 'REMOVE_COMMENT':
+            // return new state without the deleted one
+            return [
+                // from the start to the one we want to delete
+                ...state.slice(0, action.i),
+                // after the deleted one, to the end
+                ...state.slice(action.i + 1)
+            ];
+        default:
+            return state;
+    }
+    return state;
+}
+
 function comments(state = [], action) {
-  return state;
+    if (typeof action.postId !== 'undefined') {
+        return {
+            // take the current state
+            ...state,
+            // overwrite this post with a new one
+            [action.postId]: postComments(state[action.postId], action)
+        };
+    }
+    return state;
 }
 
 export default comments;
